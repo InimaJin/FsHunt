6e45924646b351eeef1169442507fd1d3efef9ab
InimaJin <>
2024-07-16 23:43:57 +0200

    Added threading, improved output & test directory

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use std::path::PathBuf;
-
 mod search;
-use search::Config;
+use search::Hunter;
 
 fn main() {
-    let config_result = Config::build();
-    let mut config: Config;
+    let config_result = Hunter::build();
+    let hunter: Hunter;
     if let Ok(c) = config_result {
-        config = c;
-        println!("{:#?}", config);
-    } else {
-        eprintln!("{}", config_result.unwrap_err());
-        std::process::exit(1);
-    }
-
-
-    // Initiate recursive search
-    search::search_dir(&config.query, PathBuf::from(config.root_path));
+        hunter = c;
+        hunter.start_search();
+    } else if let Err(msg) = config_result {
+        eprintln!("{}", msg);
+    }    
 }
--- src/search.rs
+++ src/search.rs
@@ -1,20 +1,23 @@
-use std::error::Error;
 use std::{
-    env, fs,
+    env, fs, thread,
+    path::PathBuf,
+    sync::{Mutex, Arc, mpsc}
 };
-use std::path::PathBuf;
 
-#[derive(Debug)]
-pub struct Config {
-    pub query: String,
-    pub root_path: String
+pub struct Hunter {
+    query: String, // String to search for
+    root_path: String, // Directory to start the search at
+    thread_count: u32 // Number of threads spawned
 }
 
-impl Config {
+impl Hunter {
+    // Builds a Hunter instance from the command line arguments
+    // and returns it.
     pub fn build() -> Result<Self, String> {
         let mut config = Self {
             query: String::new(),
-            root_path: String::new()
+            root_path: String::new(),
+            thread_count: 4
         };
 
         let args: Vec<String> = env::args().collect();
@@ -26,35 +29,90 @@ impl Config {
 
         Ok(config)
     }
-}
 
-pub fn search_dir(query: &str, dir: PathBuf) -> Result<(), Box<dyn Error>> {
-    let dir_contents = fs::read_dir(dir)?;
-    for result in dir_contents {
-        let dir_entry = result?;
-        let path = dir_entry.path();
-        
-        let file_name_option = path.file_name();
-        if let Some(os_str) = file_name_option {
-            let filename = String::from(os_str.to_str().unwrap());
-            if filename.contains(query) {
-                print_match(&path, query);
-            }
-        }
-
-        if path.is_dir() {
-            search_dir(query, path);
+    // Reads the elements within the root directory into a vector
+    // and passes it to self.init_threads().
+    pub fn start_search(&self) {
+        if let Ok(read_dir) = fs::read_dir(PathBuf::from(&self.root_path)) {
+            let root_contents: Vec<PathBuf> = read_dir
+                .map(|result| result.unwrap().path())
+                .collect();
+            
+            self.init_threads(root_contents);
         }
     }
 
-    Ok(())
+    /* 
+    - Creates a queue. The queue is a vector holding paths to files/ folders found during the search and
+    it is permanently being updated by the threads until all elements in the root directory (and its children) 
+    have been analyzed.
+    - Spawns threads that edit the queue and analyze the elements in it. 
+    */
+    fn init_threads(&self, root_contents: Vec<PathBuf>) {
+        let (sender, receiver) = mpsc::channel();
+        
+        let queue = Arc::new( Mutex::new(root_contents) );
+        for _ in 0..self.thread_count {
+            let sender = sender.clone();
+            let query = String::from(&self.query);
+            let queue = Arc::clone(&queue);
+            thread::spawn(move || {
+                loop {
+                    // Path to the element to be analyzed
+                    let path_from_queue = {
+                        let mut queue = queue.lock().unwrap();
+                        queue.pop()
+                    };
+                    if let Some(pathbuf) = path_from_queue {
+                        if let Some(os_str) = pathbuf.file_name() {
+                            if let Some(str) = os_str.to_str() {
+                                if str.contains(&query) {
+                                    print_match(&pathbuf, str, &query);
+                                }
+                            }
+                        }
+                        
+                        if pathbuf.is_dir() {
+                            if let Ok(read_dir) = fs::read_dir(pathbuf) {
+                                for result in read_dir {
+                                    // Sending each sub-element in the pathbuf (which is a directory)
+                                    // back to the main thread.
+                                    sender.send( result.unwrap().path() );
+                                }
+                            }
+                            
+                        }
+                    } else {
+                        // There are no elements left in the queue.
+                        break;
+                    }
+                }
+            });
+        }
+        // Dropping the original sender.
+        // Cloned versions may still exist in threads, but get dropped as threads end.
+        drop(sender);
+        // Directories sent from the spawned threads will be pushed into the queue
+        // so the threads can continue analyzing.
+        for dir in receiver {
+            let mut queue = queue.lock().unwrap();
+            queue.push(dir);
+        }
+
+    }
 }
 
-fn print_match(path: &PathBuf, query: &str) {
-    let mut path_highlight = String::new();
-    if let Some(str) = path.to_str() {
-        path_highlight.push_str(str);
-        path_highlight = path_highlight.replacen(query, &format!("\u{001b}[31m{}\u{001b}[0m", query), 1);
-        println!("{}", path_highlight);
+/*
+- Finds where the element's name matches the query.
+- Prints the element's name with the matching substring highlighted.
+*/
+fn print_match(path: &PathBuf, filename: &str, query: &str) {
+    let mut path = path.clone();
+    path.pop();
+    if let Some(_) = path.to_str() {
+        let colored_query = format!("\u{001b}[31m{}\u{001b}[0m", query);
+        let filename_highlighted = String::from(filename).replace(query, &colored_query);
+        path.push(filename_highlighted);
+        println!("{}", path.to_str().unwrap());
     }
 }
